cea050f637c9eaf5bde9d6c37465a472fb7a5067
philliplee4 <philliplee5947@gmail.com>
2025-10-11 18:08:15 -0700

    Fixed socket connection for deployment

--- js/socket-client.js
+++ js/socket-client.js
@@ -1,5 +1,12 @@
 // Connect to server
-const socket = io('http://localhost:3000');
+const SERVER_URL = window.location.hostname == 'localhost'
+    ? 'https://localhost:3000'
+    : window.location.origin;
+
+console.log('Connect to the server:', SERVER_URL);
+
+
+const socket = io(SERVER_URL);
 
 socket.on('connect', () => {
     console.log('Connected to server! ID:', socket.id);
